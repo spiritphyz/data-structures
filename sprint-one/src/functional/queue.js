e6b52b57e31937db132c6700e3f95c1e73eef555
spiritphyz <heytonyle@icloud.com>
2016-09-14 15:46:00 -0700

    Implement sequential add and remove for enqueue and dequeue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,29 @@
 var Queue = function () {
   var someInstance = {};
-
+  var counter = 0;
+  var next = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    counter ++;
+    storage[counter] = value;
   };
 
   someInstance.dequeue = function() {
+    var result = storage[next];
+    if (counter > 0) {
+      counter --;
+    }
+    delete storage[counter];
+    next ++;
+    return result;
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
