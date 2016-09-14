71af77081e1e2ce8a3f7b019232c916741f3b868
spiritphyz <heytonyle@icloud.com>
2016-09-14 14:56:01 -0700

    Implement counter for add and remove items.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,20 @@
 var Stack = function() {
   var someInstance = {};
-
+  var counter = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    counter++;
   };
 
   someInstance.pop = function() {
+    counter--;
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
