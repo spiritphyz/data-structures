5211756fd31668a250927577cb41d8ad2d8b26bc
spiritphyz <heytonyle@icloud.com>
2016-09-14 21:10:39 -0700

    Fix pseudoclassical Stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,11 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
 };
 
+  // Implement the methods below
+Stack.prototype.push = function(value) {
+
+};
 
