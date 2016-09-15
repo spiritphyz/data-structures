57aaca325b9cd5c663659d6f874bb054af1ae7bf
spiritphyz <heytonyle@icloud.com>
2016-09-15 10:27:06 -0700

    Fine tune the spec for our one test case

--- sprint-two/spec/linkedListSpec.js
+++ sprint-two/spec/linkedListSpec.js
@@ -24,11 +24,11 @@ describe('linkedList', function() {
   });
 
   it('should remove the head from the list when removeHead is called', function() {
-    linkedList.addToTail(4);
-    linkedList.addToTail(5);
-    expect(linkedList.head.value).to.equal(4);
+    linkedList.addToTail(8);
+    linkedList.addToTail(9);
+    expect(linkedList.head.value).to.equal(8);
     linkedList.removeHead();
-    expect(linkedList.head.value).to.equal(5);
+    expect(linkedList.head.value).to.equal(9);
   });
 
   it('should return the value of the former head when removeHead is called', function() {
