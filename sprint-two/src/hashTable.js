c23473c5bd5064fbf66e7c67df0afc53f7b727d7
spiritphyz <heytonyle@icloud.com>
2016-09-16 12:07:32 -0700

    Implement insert() and retrieve()

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,10 +7,32 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucketList = this._storage.get(index);
+  var tuple = [k, v];
+  if (bucketList) {
+    bucketList.push(tuple);
+  } else {
+    bucketList = [];
+    this._storage.set(index, bucketList);
+    bucketList.push(tuple);
+  }
+  console.log('this._storage ', this._storage.get(index));
+  console.log('bucketList ', bucketList);
+
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucketList = this._storage.get(index);
+  var result;
+  bucketList.forEach(function(tuple) {
+    if (tuple[0] === k) {
+      result = tuple[1];
+    } else {
+      throw 'cannot find key in bucket list';
+    }
+  }); 
+  return result;
 };
 
 HashTable.prototype.remove = function(k) {
