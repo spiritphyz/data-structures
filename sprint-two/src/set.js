cbaea01f9ec6e4ce6c57666dcb143e91b685275b
spiritphyz <heytonyle@icloud.com>
2016-09-16 12:08:11 -0700

    Add time complexity for each function

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -6,14 +6,17 @@ var Set = function() {
 
 var setPrototype = {};
 
+// time complexity: constant, O(1)
 setPrototype.add = function(item) {
   this._storage[item] = item;
 };
 
+// time complexity: constant, O(1)
 setPrototype.contains = function(item) {
   return this._storage.hasOwnProperty(item) === true; 
 };
 
+// time complexity: constant, O(1)
 setPrototype.remove = function(item) {
   delete this._storage[item];
 };
