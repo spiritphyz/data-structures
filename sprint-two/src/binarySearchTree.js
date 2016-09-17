8720579001f02f3d3b9fbd805d7964f02cd135bf
spiritphyz <heytonyle@icloud.com>
2016-09-16 19:44:57 -0700

    Implement insert functionality with correct ordering of nodes.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,36 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = undefined;
+  this.right = undefined;
 };
+
+BinarySearchTree.prototype.insert = function(val) {
+  //debugger;
+  var newNode = new BinarySearchTree(val);
+
+  var findNode = function(node) {
+    if (val > node.value) {
+      if (node.right) {
+        findNode(node.right);
+      } else {
+        node.right = newNode;
+        return;
+      }
+    } 
+    if (val < node.value) {
+      if (node.left) {
+        findNode(node.left);
+      } else {
+        node.left = newNode;
+        return;
+      }
+    }
+  }; 
+
+  findNode(this);
+};
+
+
 
 
 /*
