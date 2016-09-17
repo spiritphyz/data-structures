2dd0b4b18f14f86945c048cb1f624805816c60cd
spiritphyz <heytonyle@icloud.com>
2016-09-17 11:34:25 -0700

    Add time complexity and refactor add edges

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -13,6 +13,7 @@ Graph.prototype.addNode = function(value) {
   this.storage.push(node);
 };
 
+// Time complexity: Linear O(n)
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(value) {
   var result = false;
@@ -24,6 +25,7 @@ Graph.prototype.contains = function(value) {
   return result;
 };
 
+// Time complexity: Linear O(n)
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(value) {
   var instance = this;
@@ -41,6 +43,7 @@ Graph.prototype.removeNode = function(value) {
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
+// Time complexity: Linear O(n)
 Graph.prototype.hasEdge = function(fromNode, toNode) {
   var result = false;
   var index = this.getNodeIndex(fromNode);
@@ -60,6 +63,7 @@ Graph.prototype.getNodeIndex = function(val) {
   return result;
 };
 
+// Time complexity: Linear O(n)
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
   var instance = this;
@@ -78,30 +82,27 @@ Graph.prototype.addEdge = function(fromNode, toNode) {
   });
 };
 
+// Time complexity: Linear O(n)
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  var indexA = this.getNodeIndex(fromNode);
-  var nodeA = this.storage[indexA];
+  var index = this.getNodeIndex(fromNode);
+  var node = this.storage[index];
 
-  var position = nodeA.edges.indexOf(toNode);
-  nodeA.edges.splice(position, 1);
+  var position = node.edges.indexOf(toNode);
+  node.edges.splice(position, 1);
 
-  indexA = this.getNodeIndex(toNode);
-  nodeA = this.storage[indexA];
-  position = nodeA.edges.indexOf(fromNode);
-  nodeA.edges.splice(position, 1);
+  // remove edges from other partner
+  index = this.getNodeIndex(toNode);
+  node = this.storage[index];
+  position = node.edges.indexOf(fromNode);
+  node.edges.splice(position, 1);
 
 };
 
 // Pass in a callback which will be executed on each node of the graph.
+// Time complexity: Linear O(n)
 Graph.prototype.forEachNode = function(cb) {
   this.storage.forEach(function(node) {
     cb(node.value);
   });
 };
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
-
-
