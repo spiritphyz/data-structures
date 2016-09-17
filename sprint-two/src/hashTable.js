6f97fcdacac308736502d15093ef193a185c5871
spiritphyz <heytonyle@icloud.com>
2016-09-17 11:37:36 -0700

    Add Time Complexity

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,10 +1,10 @@
 
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
 
+// Time complexity: constant, O(1)
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucketList = this._storage.get(index);
@@ -21,6 +21,7 @@ HashTable.prototype.insert = function(k, v) {
 
 };
 
+// Time complexity: constant, O(1)
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucketList = this._storage.get(index);
@@ -33,6 +34,7 @@ HashTable.prototype.retrieve = function(k) {
   return result;
 };
 
+// Time complexity: Linear O(n)
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucketList = this._storage.get(index);
@@ -43,11 +45,3 @@ HashTable.prototype.remove = function(k) {
     }
   }); 
 };
-
-
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
-
-
