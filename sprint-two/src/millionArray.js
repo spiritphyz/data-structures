13a44a66727905bd8eca3c077fd31f26a2719f43
spiritphyz <heytonyle@icloud.com>
2016-09-17 17:12:08 -0700

    Checking profile of million array search vs. BST
    - BST is much slower, over 70% slower.
    - We think it's due to native implementation of array iteration is much faster than recursion through a million elements.
    - We should refactor our BST to use while loops instead of expensive recursive calls.

---
+++ sprint-two/src/millionArray.js
@@ -0,0 +1,19 @@
+var arrLimit = 2000000;
+var bigArr = Array.from(new Array(limit), x => Math.floor(Math.random() * limit + 1));
+
+var result = {};
+result.gap = 3000000;
+result.value = null;
+
+var target = 556;
+
+var closest = bigArr.reduce(function(memo, element) {
+  var newGap = Math.abs(element - target);
+  if (newGap < memo.gap) {
+    memo.gap = newGap;
+    memo.value = element;
+  }
+  return memo;
+}, result);
+
+console.log('closest for array struct is: ', closest.value);
